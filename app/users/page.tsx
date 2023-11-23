72e73bfcc518766d6a86cae3edce286a4f16e02d
Dezedene <gregoire.suray@gmail.com>
2023-11-23 16:45:18 +0100

    checked how cache running + client side and server side rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,17 +6,22 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const res = await fetch('https://jsonplaceholder.typicode.com/users',
+  // to store and reflesh data every 10 secs
+  // { next: { revalidate: 10 } }
+  //to not store data
+  { cache: 'no-store' }
+  )
   const users: User[] = await res.json();
 
 
   return (
     <>
-      <h1>
-        <ul>
-          { users.map(user => <li key={ user.id }>{ user.name }</li>) }
-        </ul>
-      </h1>
+      <h1>Users</h1>
+      <p>{ new Date().toLocaleTimeString() }</p>
+      <ul>
+        { users.map(user => <li key={ user.id }>{ user.name }</li>) }
+      </ul>
     </>
   )
 }
