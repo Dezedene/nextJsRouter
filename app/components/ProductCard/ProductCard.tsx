f6e8f78a64eb0be48b40faf745fe46e30b68da9c
Dezedene <gregoire.suray@gmail.com>
2023-11-24 16:19:10 +0100

    experimenting hooks

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import AddToCart from '../AddToCart'
-import styles from './ProductCard.module.css'
+// import styles from './ProductCard.module.css'
 
 
 const ProductCard = () => {
   return (
-    <div className={ styles.cardContainer }>
+    // css part
+    // <div className={ styles.cardContainer }>
+    // tailwind part
+    <div className= 'p-4 my-3 bg-sky-300 text-white text-xl hover:bg-sky-500'>
       <AddToCart />
     </div>
   )
