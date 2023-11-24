ba65c6deb4be4337ee1bb7740caa9b6414d6e742
Dezedene <gregoire.suray@gmail.com>
2023-11-24 18:10:06 +0100

    updated useEffect experimentation

--- app/components/Hooks/UseEffectTest.tsx
+++ app/components/Hooks/UseEffectTest.tsx
@@ -5,12 +5,16 @@ import React, { useState, useEffect } from 'react'
 const UseEffectTest = () => {
 
 let [resourceType, setResourceType] = useState( () => 'Posts' )
+let [ items, setItems ] = useState([])
 
 console.log('Component Render')
 
 useEffect(() => {
-  console.log('onMount')
-}, [])
+  fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    .then(response => response.json())
+    .then(json => setItems(json))
+}, [resourceType])
+
 
   return (
     <>
@@ -20,6 +24,9 @@ useEffect(() => {
         <button className='border-2 border-slate-950 p-2' onClick={ () => setResourceType('Comments') }>comments</button>
       </div>
       <h1 className='font-bold'>{ resourceType }</h1>
+      { items.map((item, i) => {
+        return <pre key={i}>{JSON.stringify(item)}</pre>
+      }) }
     </>
   )
 }
